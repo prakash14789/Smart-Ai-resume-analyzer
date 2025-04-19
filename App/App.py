195f56847d60a2cd02c7cf6d2ce984a749bfb21f
prakash14789 <151768230+prakash14789@users.noreply.github.com>
2025-04-20 00:53:06 +0530

    Update App.py

--- App/App.py
+++ App/App.py
@@ -23,6 +23,11 @@ try:
 except OSError:
     download("en_core_web_sm")
     spacy.load("en_core_web_sm")
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+
 
 
 from geopy.geocoders import Nominatim
